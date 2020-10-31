0377835287dcad292d40116f99c68a6f93a9f508
gilrsantana <g.r.santana@hotmail.com>
2020-10-31 19:16:47 -0300

    Solution comments inserted

--- Classes_e_Metodos/PrimeiroTrauma.java
+++ Classes_e_Metodos/PrimeiroTrauma.java
@@ -1,13 +1,18 @@
 package classe;
 
 public class PrimeiroTrauma {
-	static int a = 3;
+	/*
+	 * Two ways to access a variable in a class:
+	 * 1) Modify it to static, or;
+	 * 2) Create an object an than, aceess it.
+	 */
+	static int a = 3; // (1)
 	int b = 5;
 	public static void main(String[] args) {
 				
 		System.out.println(a);
 		
-		PrimeiroTrauma p1 = new PrimeiroTrauma();
+		PrimeiroTrauma p1 = new PrimeiroTrauma(); // (2)
 		System.out.println(p1.b);
 	}
 }
